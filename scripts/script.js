bffbbed3b9525b2e25c702faefd8d83ece4b44fe
ekirpichyov <47474371+ekirpichyov@users.noreply.github.com>
2019-03-30 23:56:01 +0300

    updates

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,10 @@
+//async image loading
+$(document).ready(function(){
+    $(".loadlater").each(function(index, element){
+        $(element).attr("src", $(element).attr("data-src"));
+    });
+});
+
 //slider
 
 $(document).ready(function(){
