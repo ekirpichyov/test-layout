21462f18f0846d4620c908336b6acc577b380b6e
ekirpichyov <47474371+ekirpichyov@users.noreply.github.com>
2019-03-31 00:24:48 +0300

    updates

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 //async image loading
 $(document).ready(function(){
-    $(".loadlater").each(function(index, element){
+    $(".big-image").each(function(index, element){
         $(element).attr("src", $(element).attr("data-src"));
     });
 });
